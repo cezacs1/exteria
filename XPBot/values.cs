0a28cc0f9c632ac8046fea95df0a0326329bb345
CEZA - exelent#3614 <93098721+cezacs1@users.noreply.github.com>
2023-05-09 00:29:19 +0300

    Create values.cs

---
+++ XPBot/values.cs
@@ -0,0 +1,17 @@
+namespace Exteria.NET.XPBot
+{
+    public class values
+    {
+        public static int clientstate = 0;
+        public static int gamesatate = 0;
+        
+        public static int localplayer = 0;
+        public static int localhealth = 0;
+        public static int localteam = 0;
+
+        public static int crossid = 0;
+
+        public static Vector3 localeyeposition;
+        public static Vector3 viewangles;
+    }
+}
